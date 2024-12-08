d673590a00089f2b18560e70735cfda8f0de0bcc
Marcos Alejandro Collazos Marmolejo <112360288+Archimarks@users.noreply.github.com>
2024-12-08 16:56:32 -0500

    V7

---
+++ setup3.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+sudo apt install -y zsh-syntax-highlighting
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+source /path/a/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source /path/a/zsh-autosuggestions/zsh-autosuggestions.zsh
+source ~/.zshrc
+
+sudo apt install -y zsh-autosuggestions
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+ls ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+source ~/.zshrc
