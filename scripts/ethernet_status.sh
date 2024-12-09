4f7bf82d39ac4742bb945423e7578f1593849b9c
Marcos Alejandro Collazos Marmolejo <112360288+Archimarks@users.noreply.github.com>
2024-12-09 00:26:30 -0500

    Update ethernet_status.sh
    Mejora al codigo en la optencion del ip

--- scripts/ethernet_status.sh
+++ scripts/ethernet_status.sh
@@ -3,10 +3,12 @@
 # Función para verificar si estamos en una máquina virtual
 es_maquina_virtual() {
     # Buscamos interfaces que suelen ser típicas en máquinas virtuales
-    if ip link show | grep -qE "ens33|vnet|virbr|virtio"; then
-        return 0  # Si se encuentra alguna interfaz virtual, se asume que es una máquina virtual
-    else
-        return 1  # Si no se encuentra, se asume que no es una máquina virtual
+    if ip link show | grep -qE "ens33"; then
+        return 0  # Máquina virtual
+    fi
+
+    if ip link show | grep -qE "lo"; then
+        return 1  # Máquina local
     fi
 }
 
@@ -17,13 +19,15 @@ else
     TIPO="Máquina Local"
 fi
 
-# Detectar la interfaz activa
-INTERFAZ=$(ip -o -4 addr show up | awk '{print $2}' | head -n 1)
+# Detectar la interfaz activa que no sea 'lo'
+INTERFAZ=$(ip -o -4 addr show up | grep -v " lo " | awk '{print $2}' | head -n 1)
 
 # Verificar si hay una interfaz activa con dirección IP
 if [ -n "$INTERFAZ" ]; then
-    IP=$(ip -o -4 addr show "$INTERFAZ" | awk '{print $4}' | cut -d/ -f1)
-    if echo "$INTERFAZ" | grep -qE "wlan"; then
+    # Extraer la IP utilizando ifconfig
+    IP=$(echo "%{F#ffffff}  %{F#ffffff}$(/usr/sbin/ifconfig "$INTERFAZ" | grep 'inet ' | awk '{print $2}')%{u-}")
+
+    if echo "$INTERFAZ" | grep -qE "wlan|wlx"; then
         ICONO=""  # Icono para Wi-Fi
     else
         ICONO=""  # Icono para Ethernet
